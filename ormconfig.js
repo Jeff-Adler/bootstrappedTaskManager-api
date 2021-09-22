24950c28e8367f416f81f39795cc27ffb1eabdbd
Jeff-Adler <Jeffm.adler@gmail.com>
2021-09-22 19:19:57 -0400

    began fixing migration issue

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,23 @@
-// const config = require('@/config.js');
+const config = require('@/config.js');
 
-// module.exports = {
-//   "type": "mysql",
-//   "host": "localhost",
-//   "port": 3306,
-//   "username": "test",
-//   "password": "test",
-//   "database": "test"
-// }
+const { connection_name, host, port, username, password, name } = config.get('db');
+
+module.exports = [
+  {
+    type: 'postgres',
+    name: 'default',
+    host: host,
+    port: port,
+    username: username,
+    password: password,
+    database: name,
+    synchronize: true,
+    logging: false,
+    entities: ['src/entity/*.entity.ts'],
+    migrations: ['src/migration/*.ts'],
+    migrationsRun: true,
+    cli: {
+      migrationsDir: 'src/migration'
+    }
+  }
+];
